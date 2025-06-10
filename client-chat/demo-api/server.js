da492fe335473f133f78e3f913946842164b2aec
exagyde <nolann.morence@hotmail.fr>
2025-06-11 00:18:44 +0200

    Add first nlp process

--- client-chat/demo-api/server.js
+++ client-chat/demo-api/server.js
@@ -1,8 +1,13 @@
 import express from "express";
+import { NlpManager } from "node-nlp";
 import verifyApiKey from "./utils/verifyApiKey.js";
 
 const app = express();
 const PORT = process.env.PORT || 3030;
+
+const DEFAULT_RESPONSE = "Désole, je n'ai pas la réponse.";
+const manager = new NlpManager({ languages: ["fr"], forceNER: true });
+manager.load("./nlp/model.nlp");
 
 app.use(express.json());
 app.use(verifyApiKey);
@@ -12,8 +17,9 @@ app.post("/generate", async (req, res) => {
     if (!prompt) return res.status(400).json({ error: "Empty prompt" });
 
     try {
-        const response = "API Response";
-        res.json({ result: response });
+        const response = await manager.process("fr", prompt);
+        console.log(response);
+        res.json({ result: response.answer || DEFAULT_RESPONSE });
     } catch (err) {
         res.status(500).json({ error: "Generation error", details: err.message });
     }
