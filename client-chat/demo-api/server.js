36b9c306679bf236f12d3679c43a162b5d66363c
Nolann Morence <n.morence@dimsi.fr>
2025-06-23 15:11:22 +0200

    Remove unnecessary files

--- client-chat/demo-api/server.js
+++ client-chat/demo-api/server.js
@@ -1,13 +1,10 @@
 import express from "express";
-import { NlpManager } from "node-nlp";
 import verifyApiKey from "./utils/verifyApiKey.js";
 
 const app = express();
 const PORT = process.env.PORT || 3030;
 
 const DEFAULT_RESPONSE = "Désole, je n'ai pas la réponse.";
-const manager = new NlpManager({ languages: ["fr"], forceNER: true });
-manager.load("./nlp/model.nlp");
 
 app.use(express.json());
 app.use(verifyApiKey);
@@ -17,9 +14,8 @@ app.post("/generate", async (req, res) => {
     if (!prompt) return res.status(400).json({ error: "Empty prompt" });
 
     try {
-        const response = await manager.process("fr", prompt);
-        console.log(response);
-        res.json({ result: response.answer || DEFAULT_RESPONSE });
+        const response = `Votre prompt fait ${prompt.length} caractères`;
+        res.json({ result: response || DEFAULT_RESPONSE });
     } catch (err) {
         res.status(500).json({ error: "Generation error", details: err.message });
     }
